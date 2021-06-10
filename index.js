519eac16b2b45a2448683678f52cdb580eb59cdf
nicholasgalante1997 <nicholasgalante1997@gmail.com>
2021-06-09 20:16:56 -0600

    docker-compose mysql running

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const cors = require('cors');
 const DBM = require('./core/database/DBManager');
 
 const db = new DBM({
-  host: process.env.MYSQL_LOCAL_HOST,
+  host: 'ngp-mysql',
   user: process.env.MYSQL_LOCAL_USER,
-  password: process.env.MYSQL_LOCAL_PW || '',
-  database: process.env.MYSQL_LOCAL_DB,
+  password: process.env.MYSQL_ROOT_PASSWORD || '',
+  database: process.env.MYSQL_DB,
 });
 
 const connection = db.connect(db._config);
